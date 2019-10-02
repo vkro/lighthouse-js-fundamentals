fc4311f495adfbef7e12b7396b38e11ee5f28981
Vanessa Kroeker <vkroeker@gmail.com>
2019-10-02 22:23:40 +0000

    Given two arrays, concatenate the arrays

---
+++ concat.js
@@ -0,0 +1,16 @@
+/*
+When given two arrays, concatenate the arrays together.
+*/
+
+function concat(str1, str2) {
+  str2.forEach(function(i) {
+    str1.push(i);
+  })
+  return str1;
+}
+
+
+console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]), "=?", [ 1, 2, 3, 4, 5, 6 ]);
+console.log(concat([ 0, 3, 1 ], [ 9, 7, 2 ]), "=?", [ 0, 3, 1, 9, 7, 2 ]);
+console.log(concat([], [ 9, 7, 2 ]), "=?", [ 9, 7, 2 ]);
+console.log(concat([ 5, 10 ], []), "=?", [ 5, 10 ]);
