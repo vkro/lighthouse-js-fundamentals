4593b953deb7ee72ebaf0f29b67ad8e0a11b4c10
Vanessa Kroeker <vkroeker@gmail.com>
2019-10-15 03:25:03 +0000

    refactored loopy lighthouse and changed variable name for better readability

--- loopy-lighthouse-refactored-again.js
+++ loopy-lighthouse-refactored-again.js
@@ -19,16 +19,18 @@ call with 3 parameters.
 
 function loopyLighthouse(range, multiples, words) {
   for (let i = range[0]; i <= range[1]; i++) {
-    if (i % multiples[0] === 0 && i % multiples[1] === 0) {
+    let firstMultiple = (i % multiples[0] === 0);
+    let secondMultiple = (i % multiples[1] === 0);
+
+    if (firstMultiple && secondMultiple) {
       console.log(words[0] + words[1]);
-    } else if (i % multiples[0] === 0) {
+    } else if (firstMultiple) {
       console.log(words[0]);
-    } else if (i % multiples[1] === 0) {
+    } else if (secondMultiple) {
       console.log(words[1]);
     } else console.log(i);
   }
 };
-
 
 loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]);
 //all the numbers from 15 to 90, except replacing 
